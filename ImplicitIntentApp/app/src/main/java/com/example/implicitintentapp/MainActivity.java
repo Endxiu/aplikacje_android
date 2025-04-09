6f82dd9ff8758744dda7824a3939d7a5ff87cc9d
Adrian Stańczak <10376608u@qed.local>
2025-04-09 08:10:56 +0200

    update

--- ImplicitIntentApp/app/src/main/java/com/example/implicitintentapp/MainActivity.java
+++ ImplicitIntentApp/app/src/main/java/com/example/implicitintentapp/MainActivity.java
@@ -1,6 +1,7 @@
 package com.example.implicitintentapp;
 
 import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.util.Log;
 import android.widget.Button;
@@ -29,7 +30,9 @@ public class MainActivity extends AppCompatActivity {
         Button button3 = findViewById(R.id.button3);
 
         button1.setOnClickListener(v->openWebPage("https://github.com/Endxiu/aplikacje_android"));
-
+        button2.setOnClickListener(v->dialPhoneNumber("997"));
+        //do dokonczenia
+        button3.setOnClickListener(v->sendEmail("@wp.pl"));
     }
 
     private void openWebPage(String url) {
@@ -37,8 +40,32 @@ public class MainActivity extends AppCompatActivity {
         Intent intent = new Intent(Intent.ACTION_VIEW, webpage);
 
         try {
-            startActivity(Intent);
+            startActivity(intent);
         }catch (Exception e){
+            Log.e("ERROR", "Brak aplikacji do obsługi intencji", e);
+        }
+    }
+
+    private void dialPhoneNumber(String phoneNumber) {
+        Intent intent = new Intent(Intent.ACTION_DIAL);
+        intent.setData(Uri.parse("tel:" +phoneNumber));
+
+        try {
+            startActivity(intent);
+        }catch (Exception e) {
+            Log.e("ERROR", "Brak aplikacji do obsługi intencji", e);
+        }
+    }
+
+    private void sendEmail(String address, String subject, String body) {
+        Intent intent = new Intent(Intent.ACTION_SENDTO);
+        intent.setData(Uri.parse("mail to:"));
+        intent.putExtra(Intent.EXTRA_SUBJECT, subject);
+        intent.putExtra(Intent.EXTRA_TEXT, body);
+
+        try {
+            startActivity(intent);
+        }catch (Exception e) {
             Log.e("ERROR", "Brak aplikacji do obsługi intencji", e);
         }
     }
